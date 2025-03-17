7d583324959e8762377118d01d05bf05c0505840
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-17 14:50:32 +0100

    [UPD] second if created

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-let distance = prompt("quanti chilometri devi percorrere?")
-let age = prompt("quanti anni hai?")
-let price = distance*0.21
+let distance = prompt("quanti chilometri devi percorrere?");
+let age = prompt("quanti anni hai?");
+let price = distance * 0.21;
 
-if(age>65){
-    finalprice = price*0.6
+if (age > 65) {
+    price = price * 0.6;
 }
 
-if(age<18){
-    finalprice = price*0.8
+if (age < 18) {
+    price = price * 0.8;
 }
+
