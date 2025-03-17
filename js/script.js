9b7049a9bf1036dbabaf5d5846a8f4221a32cb2d
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-17 14:33:21 +0100

    [UPD] if Created

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ let distance = prompt("quanti chilometri devi percorrere?")
 let age = prompt("quanti anni hai?")
 let price = distance*0.21
 
+if(age>65){
+    finalprice = price*0.6
+}
+
+if(age<18){
+    finalprice = price*0.8
+}
